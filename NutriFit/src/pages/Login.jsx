04806cd324a8a764cac21b019a02ab42738f88a4
y2kyg0r <ygor_luciano@estudante.sesisenai.org.br>
2025-05-22 20:52:30 -0300

    Att ygu

--- NutriFit/src/pages/Login.jsx
+++ NutriFit/src/pages/Login.jsx
@@ -7,47 +7,48 @@ const[Nome, setNome] = useState("")
 const[Senha, setSenha] = useState("")
 
 
-
   return (
     <>
       <div className='container'>
 
 
         <div className='div-esquerda'>
-        {/* <img src="/img/images.jpg" alt="" /> */}
+       
+        <div className='caixa-login'>
+
+        <h2>Faça seu Login</h2>
+
+        <input type="text" 
+        value={Nome}
+        onChange={(e) => setNome(e.target.value)}
+        className='input-nome'/>
+
+        <input type="password" 
+        value={Senha}
+        onChange={(e) => setSenha(e.target.value)}
+        className='input-senha'/>
+
+      <div className='senha-esquecida'>
+      <a href="https://passwords.google.com/intro?hl=pt">Esqueceu sua senha e/ou Usuario?</a>
+      </div>
+ 
+      <button >Login</button>
+
+
+      <div className='sem-senha'>
+      <b>Não possui uma conta?</b>
+      <label>Entre em contato pelo e-mail
+      suporte@nutrifit.com.br</label>
+ 
+      </div>
+
+      </div>
+
         </div>
 
         <div className='div-direita'>
 
-            <div className='caixa-login'>
-
-                <h2>Faça seu Login</h2>
-
-                <input type="text" 
-                value={Nome}
-                onChange={(e) => setNome(e.target.value)}
-                className='input-nome'/>
-
-                <input type="password" 
-                value={Senha}
-                onChange={(e) => setSenha(e.target.value)}
-                className='input-senha'/>
-
-                <div className='senha-esquecida'>
-                 <a href="https://passwords.google.com/intro?hl=pt">Esqueceu sua senha e/ou Usuario?</a>
-                 </div>
-                 
-                <button >Login</button>
-
-
-                <div className='sem-senha'>
-                 <b>Não possui uma conta?</b>
-                 <label>Entre em contato pelo e-mail
-                 suporte@nutrifit.com.br</label>
-                 
-                 </div>
-
-            </div>
+           {/* <img src="/img/images.jpg" alt="" /> */}
 
         </div>
 
